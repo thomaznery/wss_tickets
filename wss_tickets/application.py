a8c8c5e59a2c26239d75fc28603e5d2e17ea4766
thomaznery <thomazmichelini@hotmail.com>
2023-02-18 20:00:36 -0300

    ajust foldes/files in repository

--- wss_tickets/application.py
+++ wss_tickets/application.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask,jsonify, render_template
 from equitys import all_equitys
-from flask import jsonify
 
-application = Flask(__name__)
+
+application = Flask(__name__, template_folder="./templates")
 
 @application.route("/tickets")
 def all():
     ativos = all_equitys()    
     return jsonify(ativos)
+
+@application.route("/")
+def index():
+    return render_template("index.html")
+
+    
